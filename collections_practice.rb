720156a1053104b815eb1754d29a85ce3ee11c90
dwane richards <dwanerich@gmail.com>
2019-12-18 03:07:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,5 +10,10 @@ def sort_array_char_count(strings)
   strings.sort do |a, b|
     a.length <=> b.length
   end
+end
 
+def swap_elements(array)
+  
+  
+  array[1], array[2] = array[2], array[1]
 end
